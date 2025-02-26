556179a3129af67262a3d0a4cca545d508706430
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-02-26 09:44:07 +0800

    do data filter test

--- src/test/test_file.vb
+++ src/test/test_file.vb
@@ -4,6 +4,11 @@ Public Module test_file
 
     Sub Main()
         Dim rtp = rtpParser.readRtp("E:\emily\data\amber14sb.ff\aminoacids.rtp")
+        Dim filter As New pdbFilter(rtp)
+        Dim pdb = "F:\complex.1_backup.pdb".ReadAllLines
+        Dim filter_pdb = filter.filter(pdb).ToArray
+
+        Call filter_pdb.SaveTo("F:\complex.1.pdb")
 
         Pause()
     End Sub
