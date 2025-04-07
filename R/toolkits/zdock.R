6c995870858020ad0b1f66ae43efd1a8ae46551b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-07 10:51:28 +0800

    create commandline wrapper for the zdock

--- R/toolkits/zdock.R
+++ R/toolkits/zdock.R
@@ -1,4 +1,27 @@
-#' zdock workflow helper
-const zdock = function() {
+#' zdock workflow for docking two structure data
+#' 
+#' @param L the pdb file path 
+#' @param R the another pdb file path
+#' 
+const zdock = function(L, R, outdir = "./") {
+    let L1 = file.path(outdir, basename(L) & ".pdb");
+    let R1 = file.path(outdir, basename(R) & ".pdb");
+    let zdocklib = getOption("zdock");
+    let zdock = file.path(zdocklib,"zdock");
+    let zdock.out = file.path(outdir, "zdock.out");
 
+    zdocklib |> mark_sur(L, L1);
+    zdocklib |> mark_sur(R, R1);
+
+    system2(zdock, list(
+        "-R" = R1,
+        "-L" = L1,
+        "-o" = zdock.out 
+    ));
+
+    zdock.out;
+}
+
+const mark_sur = function(zdock, i, o) {
+    system2(`${zdock}/mark_sur`, [i o]);
 }
---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+const .onLoad = function() {
+    # set directory path that contaains zdock program
+    # config default docker environment.
+    options(zdock = "/usr/local/bin/");
+}
