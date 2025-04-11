c3618499e271665ccd0d86dc5c0a0552c9eaf6d8
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-11 14:46:12 +0800

    configs of the drawing api

--- src/emily/Rscript.vb
+++ src/emily/Rscript.vb
@@ -1,7 +1,14 @@
 ﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports PDB_canvas
+Imports SMRUCC.genomics.Data.RCSB.PDB
 
 <Package("Rscript")>
 Module Rscript
 
+    <ExportAPI("draw_pdb")>
+    Public Function draw_pdb(pdb As PDB, Optional size As Object = "3000,2100") As Object
+        Return DrawingPDB.MolDrawing(pdb,)
+    End Function
 End Module
