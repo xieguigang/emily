2ef0816929c5a1e4ffd2079f08f06b48680e227c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-11 13:52:12 +0800

    fix of the method reference error

--- src/PDB_canvas/DrawingPDB.vb
+++ src/PDB_canvas/DrawingPDB.vb
@@ -42,27 +42,47 @@
 #End Region
 
 Imports System.Drawing
-Imports System.Runtime.CompilerServices
-Imports Microsoft.VisualBasic.CommandLine.Reflection
-Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.SchemaMaps
 Imports Microsoft.VisualBasic.Drawing
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors.Scaler
 Imports Microsoft.VisualBasic.Imaging.Drawing3D.Math3D
-Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports Microsoft.VisualBasic.Imaging.Driver
 Imports SMRUCC.genomics.Data.RCSB.PDB
+
+#If NET48 Then
+Imports Font = System.Drawing.Font
+Imports Pen = System.Drawing.Pen
+Imports Brush = System.Drawing.Brush
+Imports Image = System.Drawing.Image
+Imports Pens = System.Drawing.Pens
+Imports Brushes = System.Drawing.Brushes
+#Else
+Imports Font = Microsoft.VisualBasic.Imaging.Font
+Imports Pen = Microsoft.VisualBasic.Imaging.Pen
+Imports Brush = Microsoft.VisualBasic.Imaging.Brush
+Imports Image = Microsoft.VisualBasic.Imaging.Image
+Imports Pens = Microsoft.VisualBasic.Imaging.Pens
+Imports Brushes = Microsoft.VisualBasic.Imaging.Brushes
+#End If
 
 ''' <summary>
 ''' Visualize the protein 3D structure from the PDB file.
 ''' </summary>
-<Package("Drawing.Protein.3D.PDB",
-                    Description:="Visualize the protein 3D structure from the PDB file.",
-                    Publisher:="amethyst.asuka@gcmodeller.org", Url:="http://gcmodeller.org")>
-Public Module DrawingPDB
+Public Class DrawingPDB
 
-    <DataFrameColumn("X.Rotation")> Public XRotation As Double = 60
-    <DataFrameColumn("Scale.Factor")> Public ScaleFactor As Double = 20
-    <DataFrameColumn("Pen.Width")> Public penWidth As Integer = 10
+    Public Property XRotation As Double = 60
+    Public Property ScaleFactor As Double = 20
+    Public Property penWidth As Integer = 10
+
+    Shared Sub New()
+#If NET8_0_OR_GREATER Then
+        Call Microsoft.VisualBasic.Drawing.SkiaDriver.Register()
+#End If
+    End Sub
+
+    Sub New()
+
+    End Sub
 
     ''' <summary>
     ''' Drawing a protein structure from its pdb data.
@@ -71,11 +91,9 @@ Public Module DrawingPDB
     ''' <param name="hideAtoms"></param>
     ''' <param name="DisplayAAID"></param>
     ''' <returns></returns>
-    <ExportAPI("Drawing.Invoke", Info:="Drawing a protein structure from its pdb data.")>
-    <Extension>
-    Public Function MolDrawing(PDB As PDB, Optional hideAtoms As Boolean = True, Optional DisplayAAID As Boolean = True) As Image
-        Dim Device As Graphics2D = (New Size(3000, 3000)).CreateGDIDevice
-        Dim offset As Point = Device.Center
+    Public Function MolDrawing(PDB As PDB, Optional hideAtoms As Boolean = True, Optional DisplayAAID As Boolean = True) As GraphicsData
+        Dim Device As IGraphics = DriverLoad.CreateGraphicsDevice(New Size(3000, 3000))
+        Dim offset As New Point(Device.Width / 2, Device.Height / 2)
         Dim AASequence As AminoAcid() = PDB.AminoAcidSequenceData
         Dim PreAA As AminoAcid = AASequence.First
         Dim PrePoint As PointF
@@ -90,7 +108,7 @@ Public Module DrawingPDB
 
 
             Call __drawingOfAA(Point, pt2d, offset, Device, DisplayAAID, AAFont, hideAtoms)
-            Call Device.Graphics.DrawLine(AAColors(Point.AA_ID), pt2d, PrePoint)
+            Call Device.DrawLine(AAColors(Point.AA_ID), pt2d, PrePoint)
 
             PrePoint = pt2d
         Next
@@ -98,22 +116,21 @@ Public Module DrawingPDB
         Dim Max = PDB.MaxSpace
         Dim Min = PDB.MinSpace
 
-        Call Device.Graphics.DrawLine(Pens.Black, New Drawing3D.Point3D(Max.X * ScaleFactor, 0, 0).SpaceToGrid(XRotation, offset), New Drawing3D.Point3D(Min.Y * ScaleFactor, 0, 0).SpaceToGrid(XRotation, offset)) 'X
-        Call Device.Graphics.DrawLine(Pens.Black, New Drawing3D.Point3D(0, Max.Y * ScaleFactor, 0).SpaceToGrid(XRotation, offset), New Drawing3D.Point3D(0, Min.Y * ScaleFactor, 0).SpaceToGrid(XRotation, offset)) 'Y
-        Call Device.Graphics.DrawLine(Pens.Black, New Drawing3D.Point3D(0, 0, Max.Z * ScaleFactor).SpaceToGrid(XRotation, offset), New Drawing3D.Point3D(0, 0, Min.Z * ScaleFactor).SpaceToGrid(XRotation, offset)) 'Z
+        Call Device.DrawLine(Pens.Black, New Drawing3D.Point3D(Max.X * ScaleFactor, 0, 0).SpaceToGrid(XRotation, offset), New Drawing3D.Point3D(Min.Y * ScaleFactor, 0, 0).SpaceToGrid(XRotation, offset)) 'X
+        Call Device.DrawLine(Pens.Black, New Drawing3D.Point3D(0, Max.Y * ScaleFactor, 0).SpaceToGrid(XRotation, offset), New Drawing3D.Point3D(0, Min.Y * ScaleFactor, 0).SpaceToGrid(XRotation, offset)) 'Y
+        Call Device.DrawLine(Pens.Black, New Drawing3D.Point3D(0, 0, Max.Z * ScaleFactor).SpaceToGrid(XRotation, offset), New Drawing3D.Point3D(0, 0, Min.Z * ScaleFactor).SpaceToGrid(XRotation, offset)) 'Z
 
         Return Device.ImageResource
     End Function
 
-    <Extension>
     Private Sub __drawingOfAA(AA As AminoAcid, ByRef pt2d As PointF, offset As Point, Device As Graphics2D, DisplayAAID As Boolean, AAFont As Font, hideAtoms As Boolean)
         Dim Carbon As Keywords.AtomUnit = AA.Carbon
         Dim pt3d As New Drawing3D.Point3D(Carbon.Location.X * ScaleFactor, Carbon.Location.Y * ScaleFactor, Carbon.Location.Z * ScaleFactor)
         pt2d = pt3d.SpaceToGrid(xRotate:=XRotation, offset:=offset)
-        Call Device.Graphics.FillEllipse(Brushes.Black, New RectangleF(pt2d, New Size(penWidth, penWidth)))
+        Call Device.FillEllipse(Brushes.Black, New RectangleF(pt2d, New Size(penWidth, penWidth)))
 
         If DisplayAAID Then
-            Call Device.Graphics.DrawString(AA.AA_ID, AAFont, Brushes.Gray, pt2d)
+            Call Device.DrawString(AA.AA_ID, AAFont, Brushes.Gray, pt2d)
         End If
 
         If hideAtoms Then Return
@@ -121,7 +138,7 @@ Public Module DrawingPDB
         For Each Atom In AA.Atoms
             Dim pt = New Drawing3D.Point3D(Atom.Location.X * ScaleFactor, Atom.Location.Y * ScaleFactor, Atom.Location.Z * ScaleFactor)
             Dim pt22d = pt.SpaceToGrid(XRotation, offset)
-            Call Device.Graphics.DrawLine(Pens.Gray, pt22d, pt2d)
+            Call Device.DrawLine(Pens.Gray, pt22d, pt2d)
         Next
     End Sub
-End Module
+End Class
