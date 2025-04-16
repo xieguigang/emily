0882720c498a9b1b5f49e3bd7af04096aa661586
谢桂纲 <gg.xie@bionovogene.com>
2025-04-16 15:40:32 +0800

    Update test_pdb.R

--- test/test_pdb.R
+++ test/test_pdb.R
@@ -1,4 +1,4 @@
 require(emily);
 
 
-print(smiles_2_pdb(smiles = "CC(COC)CC", db_xref = "NA"));
+print(smiles_2_pdb(smiles = "COC1=C(C=C(C=C1)CC2COC(=O)C2CC3=CC(=C(C=C3)OC4C(C(C(C(O4)CO)O)O)O)OC)OC", db_xref = "NA"));
