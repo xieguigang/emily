6b774f350d3b4ed529afadcb7d83b2b7c6416b5b
谢桂纲 <gg.xie@bionovogene.com>
2025-04-16 14:13:13 +0800

    create R package project

---
+++ @export/index.d.ts
@@ -0,0 +1,23 @@
+﻿// export R# source type define for javascript/typescript language
+//
+// package_source=emily
+
+declare namespace emily {
+   module _ {
+      /**
+      */
+      function onLoad(): object;
+   }
+   /**
+   */
+   function mark_sur(zdock: any, i: any, o: any): object;
+   /**
+     * @param db_xref default value Is ``NA``.
+     * @param openbabel default value Is ``Call "getOption"("openbabel")``.
+   */
+   function smiles_2_pdb(smiles: any, db_xref?: any, openbabel?: any): object;
+   /**
+     * @param outdir default value Is ``./``.
+   */
+   function zdock(L: any, R: any, outdir?: any): object;
+}
