3ea4a13e957fb062006f1584d1f4d52f08128328
谢桂纲 <gg.xie@bionovogene.com>
2025-04-16 17:24:43 +0800

    print to stdoutput

--- scripts/smiles2pdb.py
+++ scripts/smiles2pdb.py
@@ -3,7 +3,7 @@ import sys
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
-def smiles_to_pdb(smiles, output_file):
+def smiles_to_pdb(smiles, output_file = None):
     try:
         # 转换流程
         mol = Chem.MolFromSmiles(smiles)
@@ -11,17 +11,28 @@ def smiles_to_pdb(smiles, output_file):
         AllChem.EmbedMolecule(mol, randomSeed=0xf00d)  # 生成3D坐标
         AllChem.MMFFOptimizeMolecule(mol)  # 力场优化
         
-        # 写入PDB文件
-        print(Chem.MolToPDBBlock(mol))
+        if output_file is None:
+            print(Chem.MolToPDBBlock(mol))
+        else:
+            # 写入PDB文件
+            Chem.MolToPDBFile(mol, output_file)
+            print(f"Success: {smiles} → {output_file}")
+
         return 0
     except Exception as e:
         print(f"Error: {str(e)}")
         return 1
 
 if __name__ == "__main__":
+    
     if len(sys.argv) == 1:
         print("Usage: smiles2pdb.py <SMILES> [<OUTPUT.pdb>]")
         sys.exit(1)
-    
-    _, smiles, output = sys.argv
-    sys.exit(smiles_to_pdb(smiles, output))
+    else:
+
+        if len(sys.argv) == 2:
+            _, smiles = sys.argv
+            sys.exit(smiles_to_pdb(smiles, None))
+        else:
+            _, smiles, output = sys.argv
+            sys.exit(smiles_to_pdb(smiles, output))
