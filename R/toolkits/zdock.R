2eab94cb90ef006da23cfaef5d3d79e76b7f4893
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-11 14:26:14 +0800

    Update zdock.R

--- R/toolkits/zdock.R
+++ R/toolkits/zdock.R
@@ -12,13 +12,19 @@ const zdock = function(L, R, outdir = "./", fixed = FALSE, n = 10) {
     let R1 = file.path(outdir, basename(R) & ".pdb");
     let zdocklib = getOption("zdock");
     let zdock = file.path(zdocklib,"zdock");
-    let zdock.out = file.path(outdir, "zdock.out");
+    let mk_complex = file.path(zdocklib,"create.pl");
+    let create_lig = file.path(zdocklib,"create_lig");
+    let zdock.out = "./zdock.out";
+    let workdir = getwd();
 
     # Standard PDB format files must be processed by mark_sur to add atom radius, 
     # charge and atom type. If you know that some atoms are not in the binding site, 
     # you can block them by changing their atom type (column 55-56) to 19.
     zdocklib |> mark_sur(L, L1);
     zdocklib |> mark_sur(R, R1);
+
+    setwd(outdir);
+    file.copy(create_lig, outdir & "/");
 
     # argument options should keeps the input order
     # Usage: zdock [options] -R [receptor.pdb] -L [ligand.pdb]
@@ -30,7 +36,17 @@ const zdock = function(L, R, outdir = "./", fixed = FALSE, n = 10) {
         "-L" = L1        
     ));
 
-    zdock.out;
+    if (file.exists(zdock.out)) {
+        system2(mk_complex, zdock.out);
+
+        let complex.1.pdb = readText("./complex.1.pdb");
+        let zdocking = readText(zdock.out);
+        setwd(workdir);
+        list(zdock = zdocking, pdb_txt = complex.1.pdb);
+    } else {
+        setwd(workdir);
+        invisible(NULL);
+    }
 }
 
 const mark_sur = function(zdock, i, o) {
