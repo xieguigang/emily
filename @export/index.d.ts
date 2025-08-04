cd7e4d11cf6456cdb9f1d87fa2dcea62d41c3704
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-04 09:35:42 +0800

    run package msbuild

--- @export/index.d.ts
+++ @export/index.d.ts
@@ -24,6 +24,9 @@ declare namespace emily {
    */
    function smiles_2_pdb_rdkit(smiles: any, db_xref?: any, rdkit?: any, debug?: any): object;
    /**
+   */
+   function view_pdb(pdb_txt: any, save_file: any): object;
+   /**
      * @param outdir default value Is ``./``.
      * @param fixed default value Is ``false``.
      * @param n default value Is ``10``.
