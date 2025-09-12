5ccd438953ea270e02b9e9bee16d037884583b24
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-12 16:04:37 +0800

    draw ligand molecule  success

--- src/PDB_canvas/Ligand2DPlot.vb
+++ src/PDB_canvas/Ligand2DPlot.vb
@@ -1,9 +1,12 @@
 ﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
+Imports Microsoft.VisualBasic.Data.ChartPlots
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Legend
 Imports Microsoft.VisualBasic.Data.ChartPlots.Plot3D.Device
 Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.d3js.scale
 Imports Microsoft.VisualBasic.Imaging.Drawing2D
 Imports Microsoft.VisualBasic.Imaging.Drawing3D
 Imports Microsoft.VisualBasic.Imaging.Drawing3D.Math3D
@@ -11,6 +14,7 @@ Imports Microsoft.VisualBasic.Imaging.Math2D
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.MIME.Html.CSS
 Imports Microsoft.VisualBasic.MIME.Html.Render
+Imports Microsoft.VisualBasic.Scripting.Runtime
 Imports SMRUCC.genomics.Data.RCSB.PDB
 Imports SMRUCC.genomics.Data.RCSB.PDB.Keywords
 
@@ -63,8 +67,21 @@ Public Class Ligand2DPlot : Inherits Plot
             .Where(Function(a) a.dist < 5) _
             .OrderBy(Function(a) a.dist) _
             .ToArray
-        Dim atoms As New List(Of AtomModel)
+        Dim atoms As New List(Of Element3D)
         Dim camera As New Camera(canvas, New Drawing3D.Point3D())
+        Dim connect = pdb.Conect.AsEnumerable.ToDictionary(Function(a) a.name, Function(a) a.value)
+        Dim atomIndex = hetAtoms.ToDictionary(Function(a) a.AtomNumber.ToString)
+        Dim linkStroke As New Pen(Color.Green, 5)
+
+        For Each link In connect
+            For Each t2 In link.Value.AsCharacter
+                If atomIndex.ContainsKey(link.Key) AndAlso atomIndex.ContainsKey(t2) Then
+                    Call atoms.Add(New Plot3D.Device.Line(atomIndex(link.Key), atomIndex(t2)) With {
+                        .Stroke = linkStroke
+                    })
+                End If
+            Next
+        Next
 
         For Each atom As IGrouping(Of String, (atom As HETATM.HETATMRecord, aa As AtomUnit, dist As Double)) In knn.GroupBy(Function(a) a.atom.AtomName)
             Dim ligand = atom.First.atom
@@ -72,7 +89,7 @@ Public Class Ligand2DPlot : Inherits Plot
             Call atoms.Add(New AtomModel With {
                 .Fill = Brushes.Black,
                 .IsResidue = False,
-                .Label = ligand.ResidueName,
+                .Label = ligand.ElementSymbol,
                 .Size = New Size(20, 20),
                 .Style = LegendStyles.Circle,
                 .Location = New Drawing3D.Point3D(ligand.XCoord, ligand.YCoord, ligand.ZCoord)
@@ -90,7 +107,7 @@ Public Class Ligand2DPlot : Inherits Plot
             Next
         Next
 
-        For Each element As AtomModel In atoms
+        For Each element As Element3D In atoms
             Call element.Transform(camera)
         Next
 
@@ -109,12 +126,19 @@ Public Class Ligand2DPlot : Inherits Plot
         Dim orders = PainterAlgorithm _
             .OrderProvider(atoms, Function(e) e.Location.Z) _
             .ToArray
+        Dim fontSize As New DoubleRange(12, 36)
+        Dim offset As New DoubleRange(0, orders.Length)
 
+        ' 靠前的原子是比较远的原子
         For i As Integer = 0 To atoms.Count - 1
             Dim index As Integer = orders(i)
             Dim model As Element3D = atoms(index)
 
-            Call model.Draw(g, canvas, scaleX, scaleY)
+            If TypeOf model Is AtomModel Then
+                DirectCast(model, AtomModel).fontSize = offset.ScaleMapping(i, fontSize)
+            End If
+
+            model.Draw(g, canvas, scaleX, scaleY)
         Next
     End Sub
 End Class
@@ -122,5 +146,15 @@ End Class
 Public Class AtomModel : Inherits ShapePoint
 
     Public Property IsResidue As Boolean
+    Public Property fontSize As Double
+
+    Public Overrides Sub Draw(g As IGraphics, rect As GraphicsRegion, scaleX As LinearScale, scaleY As LinearScale)
+        Dim praw As PointF = GetPosition(rect.Size)
+        Dim pscale As New PointF(scaleX(praw.X), scaleY(praw.Y))
+        Dim font As New Font(FontFace.SegoeUI, FontFace.PointSizeScale(fontSize, g.Dpi))
+
+        Call g.DrawLegendShape(pscale, Size, Style, Fill)
+        Call g.DrawString(Label, font, Brushes.Blue, pscale)
+    End Sub
 
 End Class
