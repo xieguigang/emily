520efc6d5a266cd9dac1eb7204c282413cae5340
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-15 16:42:42 +0800

    apply style

---
+++ src/PDB_canvas/Ligand2DPlot/RenderArguments.vb
@@ -0,0 +1,48 @@
+﻿Imports System.ComponentModel
+
+Public Class RenderArguments
+
+    <Category("3D Camera")>
+    <DisplayName("View X")>
+    <Description("3D camera view point for make the 3d pdb model to 2d plot projection")>
+    Public Property viewX As Double
+    <Category("3D Camera")>
+    <DisplayName("View Y")>
+    <Description("3D camera view point for make the 3d pdb model to 2d plot projection")>
+    Public Property viewY As Double
+    <Category("3D Camera")>
+    <DisplayName("View Z")>
+    <Description("3D camera view point for make the 3d pdb model to 2d plot projection")>
+    Public Property viewZ As Double
+
+    <Category("Ligand Model")>
+    <DisplayName("Distance Cutoff")>
+    <Description("The distance cutoff value of ligand atom to the amino acid residue, in data unit Å, default value is 3.5Å")>
+    Public Property DistanceCutoff As Double = 3.5
+    <Category("Ligand Model")>
+    <DisplayName("KNN")>
+    <Description("Select closed top n ligand atom and amino acid residue for display the interaction. defult choose the top one closest.")>
+    Public Property TopRank As Integer = 1
+    <Category("Ligand Render")>
+    <DisplayName("Ligand Atom Size")>
+    <Description("The shape size of the ligand atom.")>
+    Public Property AtomSize As Single = 95
+    <Category("Ligand Render")>
+    <DisplayName("Amino Acid Size")>
+    <Description("The shape size of the amino acid residue.")>
+    Public Property AminoAcidSize As Single = 150
+
+    <Category("Ligand Render")>
+    <DisplayName("Show Liagnd Atom Label")>
+    <Description("Show or hide the ligand molecule atom label on the graphics plot?")>
+    Public Property ShowLigandAtomLabel As Boolean = True
+    <Category("Ligand Render")>
+    <DisplayName("Amino Acid Min Font Size")>
+    <Description("The min size of the font label for show the amino acid residue label")>
+    Public Property AminoAcidFontSizeMin As Double = 12
+    <Category("Ligand Render")>
+    <DisplayName("Amino Acid Max Font Size")>
+    <Description("The max size of the font label for show the amino acid residue label")>
+    Public Property AminoAcidFontSizeMax As Double = 40
+
+End Class
--- src/emily/Rscript.vb
+++ src/emily/Rscript.vb
@@ -2,6 +2,7 @@
 Imports Emily.PDB_Canvas
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
+Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Driver
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.genomics.Data.RCSB.PDB
@@ -19,6 +20,7 @@ Module Rscript
 
     <ExportAPI("draw_ligand2D")>
     Public Function draw_ligand2d(pdb As PDB, ligand As Het.HETRecord,
+                                  Optional style As RenderArguments = Nothing,
                                   <RRawVectorArgument>
                                   Optional size As Object = "3600,2400",
                                   Optional dpi As Integer = 120,
@@ -29,7 +31,25 @@ Module Rscript
         Dim plotSize As String = InteropArgumentHelper.getSize(size, env, [default]:="3600,2400")
         Dim driver As Drivers = env.getDriver
 
+        If style Is Nothing Then
+            style = New RenderArguments
+        End If
+
         Call render.CalculateMaxPlainView()
+
+        If style.viewX <> 0.0 OrElse style.viewY <> 0.0 OrElse style.viewZ <> 0.0 Then
+            render.ViewPoint = New Drawing3D.Point3D(style.viewX, style.viewY, style.viewZ)
+        End If
+
+        render.ShowAtomLabel = style.ShowLigandAtomLabel
+        render.FontSizeMax = style.AminoAcidFontSizeMax
+        render.FontSizeMin = style.AminoAcidFontSizeMin
+        render.AminoAcidSize = style.AminoAcidSize
+        render.AtomSize = style.AtomSize
+        render.DistanceCutoff = style.DistanceCutoff
+        render.TopRank = style.TopRank
+
+        Call render.Build3DModel()
 
         Return render.Plot(plotSize, dpi, driver)
     End Function
