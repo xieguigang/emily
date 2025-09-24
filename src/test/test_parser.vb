8b96993dfd25dd59efb835d9f845e4f30a4c97a0
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-24 14:20:34 +0800

    fix of the parser error

---
+++ src/test/test_parser.vb
@@ -0,0 +1,10 @@
+﻿Imports SMRUCC.genomics.Data
+Imports SMRUCC.genomics.Data.RCSB.PDB
+
+Module test_parser
+
+    Sub test_reader()
+        Dim pdbqt As PDB = RCSB.PDB.PDB.Load("G:\emily\test\vina\result.pdbqt")
+        Dim pdb As PDB = PDB.Load("G:\emily\data\8qne.pdb")
+    End Sub
+End Module
