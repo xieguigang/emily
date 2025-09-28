be18f65e0c0afe3167faa5b243a0396f3cb40be2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-09-28 15:28:42 +0800

    Update vina_split.R

--- R/toolkits/autodock_vina/vina_split.R
+++ R/toolkits/autodock_vina/vina_split.R
@@ -4,12 +4,17 @@
 #' @param scores a dataframe that contains thescore that parsed from the vina docking result log output text
 #' @param prot_pdbqt the content text data of the protein receptor input of the vina docking.
 #' 
+#' @return a dataframe object that has append with the ``pdbqt`` data field:
+#'    the models output from the autodock vina docking result.
+#' 
 const split_pdbqt = function(scores, result_pdbqt, prot_pdbqt = NULL) {
     let models = vina_split(result_pdbqt |> readText());
     
     if (nchar(prot_pdbqt) > 0) {
         # simply combine the pdbqt text data 
-        # as the complex result
+        # as the protein-ligand complex result model
+        # save this character vector as pdbqt files
+        # which could be open and view in mol* web application
         models = `${models}${prot_pdbqt}`;
     }
 
