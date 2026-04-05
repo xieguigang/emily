7beb399433036611d9ac37ae0b482ef7bfb9b2dc
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-05 15:59:44 +0800

    split spoiurce file into multiple parts

---
+++ src/eQuilibrator/Compound/EquilibratorDatabase/CompoundExtensions.vb
@@ -0,0 +1,106 @@
+﻿Imports System.Text
+
+Namespace EquilibratorThermodynamics
+
+    ' ========================================================================
+    ' 扩展方法
+    ' ========================================================================
+
+    Public Module CompoundExtensions
+
+        ''' <summary>
+        ''' 获取化合物的分子式
+        ''' </summary>
+        <System.Runtime.CompilerServices.Extension>
+        Public Function GetMolecularFormula(compound As Compound) As String
+            If compound.AtomBag Is Nothing OrElse compound.AtomBag.Count = 0 Then
+                Return String.Empty
+            End If
+
+            ' 按照Hill系统排序: C先，H次之，其他按字母顺序
+            Dim sb As New StringBuilder()
+
+            ' 碳
+            If compound.AtomBag.ContainsKey("C") Then
+                sb.Append("C")
+                If compound.AtomBag("C") > 1 Then
+                    sb.Append(compound.AtomBag("C"))
+                End If
+            End If
+
+            ' 氢
+            If compound.AtomBag.ContainsKey("H") Then
+                sb.Append("H")
+                If compound.AtomBag("H") > 1 Then
+                    sb.Append(compound.AtomBag("H"))
+                End If
+            End If
+
+            ' 其他元素按字母顺序
+            Dim otherElements = compound.AtomBag.Keys.Where(Function(k) k <> "C" AndAlso k <> "H").OrderBy(Function(k) k)
+            For Each element In otherElements
+                sb.Append(element)
+                If compound.AtomBag(element) > 1 Then
+                    sb.Append(compound.AtomBag(element))
+                End If
+            Next
+
+            Return sb.ToString()
+        End Function
+
+        ''' <summary>
+        ''' 获取化合物的净电荷
+        ''' </summary>
+        <System.Runtime.CompilerServices.Extension>
+        Public Function GetNetCharge(compound As Compound) As Integer
+            If compound.Microspecies Is Nothing OrElse compound.Microspecies.Count = 0 Then
+                Return 0
+            End If
+
+            Dim majorMs = compound.Microspecies.FirstOrDefault(Function(m) m.IsMajor)
+            If majorMs IsNot Nothing Then
+                Return majorMs.Charge
+            End If
+
+            Return 0
+        End Function
+
+        ''' <summary>
+        ''' 获取化合物的pKa值列表
+        ''' </summary>
+        <System.Runtime.CompilerServices.Extension>
+        Public Function GetpKaValues(compound As Compound) As List(Of Double)
+            If compound.DissociationConstants Is Nothing OrElse compound.DissociationConstants.Count = 0 Then
+                Return New List(Of Double)()
+            End If
+
+            ' 解离常数转换为pKa
+            Return compound.DissociationConstants.Select(Function(k) -Math.Log10(k)).ToList()
+        End Function
+
+        ''' <summary>
+        ''' 获取化合物的简要描述
+        ''' </summary>
+        <System.Runtime.CompilerServices.Extension>
+        Public Function GetDescription(compound As Compound) As String
+            Dim sb As New StringBuilder()
+            sb.AppendLine($"化合物ID: {compound.Id}")
+            sb.AppendLine($"InChI Key: {compound.InChIKey}")
+            sb.AppendLine($"SMILES: {compound.Smiles}")
+            sb.AppendLine($"分子量: {compound.Mass:F3} g/mol")
+            sb.AppendLine($"分子式: {compound.GetMolecularFormula()}")
+            sb.AppendLine($"净电荷: {compound.GetNetCharge()}")
+
+            If compound.Microspecies IsNot Nothing AndAlso compound.Microspecies.Count > 0 Then
+                sb.AppendLine($"微物种数: {compound.Microspecies.Count}")
+            End If
+
+            If compound.GroupVector IsNot Nothing AndAlso compound.GroupVector.Length > 0 Then
+                sb.AppendLine($"基团向量维度: {compound.GroupVector.Length}")
+            End If
+
+            Return sb.ToString()
+        End Function
+
+    End Module
+End Namespace
--- src/eQuilibrator/Compound/EquilibratorDatabase/EquilibratorDatabaseReader.vb
+++ src/eQuilibrator/Compound/EquilibratorDatabase/EquilibratorDatabaseReader.vb
@@ -5,6 +5,7 @@
 
 Imports System.Data
 Imports System.Text
+Imports Microsoft.VisualBasic.Data.IO.ManagedSqlite.Core
 
 Namespace EquilibratorThermodynamics
 
@@ -12,42 +13,22 @@ Namespace EquilibratorThermodynamics
     ''' SQLite数据库读取器
     ''' 用于读取equilibrator_api的化合物数据库
     ''' </summary>
-    Public Class EquilibratorDatabaseReader
-        Implements IDisposable
+    Public Class EquilibratorDatabaseReader : Implements IDisposable
 
-        ' Private _connection As SQLiteConnection
+        Private _connection As Sqlite3Database
         Private _disposed As Boolean = False
 
         ''' <summary>
         ''' 数据库文件路径
         ''' </summary>
-        Public Property DatabasePath As String
+        Public ReadOnly Property DatabasePath As String
 
         ''' <summary>
         ''' 构造函数
         ''' </summary>
         Public Sub New(databasePath As String)
-            DatabasePath = databasePath
-            Dim connectionString As String = $"Data Source={databasePath};Version=3;Read Only=True;"
-            '    _connection = New SQLiteConnection(connectionString)
-        End Sub
-
-        ''' <summary>
-        ''' 打开数据库连接
-        ''' </summary>
-        Public Sub Open()
-            'If _connection.State <> ConnectionState.Open Then
-            '    _connection.Open()
-            'End If
-        End Sub
-
-        ''' <summary>
-        ''' 关闭数据库连接
-        ''' </summary>
-        Public Sub Close()
-            'If _connection.State = ConnectionState.Open Then
-            '    _connection.Close()
-            'End If
+            _DatabasePath = databasePath
+            _connection = Sqlite3Database.OpenFile(databasePath, False)
         End Sub
 
         ''' <summary>
@@ -355,15 +336,14 @@ Namespace EquilibratorThermodynamics
 #Region "IDisposable Support"
 
         Protected Overridable Sub Dispose(disposing As Boolean)
-            'If Not _disposed Then
-            '    If disposing Then
-            '        If _connection IsNot Nothing Then
-            '            _connection.Close()
-            '            _connection.Dispose()
-            '        End If
-            '    End If
-            '    _disposed = True
-            'End If
+            If Not _disposed Then
+                If disposing Then
+                    If _connection IsNot Nothing Then
+                        _connection.Dispose()
+                    End If
+                End If
+                _disposed = True
+            End If
         End Sub
 
         Public Sub Dispose() Implements IDisposable.Dispose
@@ -374,107 +354,5 @@ Namespace EquilibratorThermodynamics
 #End Region
 
     End Class
-
-    ' ========================================================================
-    ' 扩展方法
-    ' ========================================================================
-
-    Public Module CompoundExtensions
-
-        ''' <summary>
-        ''' 获取化合物的分子式
-        ''' </summary>
-        <System.Runtime.CompilerServices.Extension>
-        Public Function GetMolecularFormula(compound As Compound) As String
-            If compound.AtomBag Is Nothing OrElse compound.AtomBag.Count = 0 Then
-                Return String.Empty
-            End If
-
-            ' 按照Hill系统排序: C先，H次之，其他按字母顺序
-            Dim sb As New StringBuilder()
-
-            ' 碳
-            If compound.AtomBag.ContainsKey("C") Then
-                sb.Append("C")
-                If compound.AtomBag("C") > 1 Then
-                    sb.Append(compound.AtomBag("C"))
-                End If
-            End If
-
-            ' 氢
-            If compound.AtomBag.ContainsKey("H") Then
-                sb.Append("H")
-                If compound.AtomBag("H") > 1 Then
-                    sb.Append(compound.AtomBag("H"))
-                End If
-            End If
-
-            ' 其他元素按字母顺序
-            Dim otherElements = compound.AtomBag.Keys.Where(Function(k) k <> "C" AndAlso k <> "H").OrderBy(Function(k) k)
-            For Each element In otherElements
-                sb.Append(element)
-                If compound.AtomBag(element) > 1 Then
-                    sb.Append(compound.AtomBag(element))
-                End If
-            Next
-
-            Return sb.ToString()
-        End Function
-
-        ''' <summary>
-        ''' 获取化合物的净电荷
-        ''' </summary>
-        <System.Runtime.CompilerServices.Extension>
-        Public Function GetNetCharge(compound As Compound) As Integer
-            If compound.Microspecies Is Nothing OrElse compound.Microspecies.Count = 0 Then
-                Return 0
-            End If
-
-            Dim majorMs = compound.Microspecies.FirstOrDefault(Function(m) m.IsMajor)
-            If majorMs IsNot Nothing Then
-                Return majorMs.Charge
-            End If
-
-            Return 0
-        End Function
-
-        ''' <summary>
-        ''' 获取化合物的pKa值列表
-        ''' </summary>
-        <System.Runtime.CompilerServices.Extension>
-        Public Function GetpKaValues(compound As Compound) As List(Of Double)
-            If compound.DissociationConstants Is Nothing OrElse compound.DissociationConstants.Count = 0 Then
-                Return New List(Of Double)()
-            End If
-
-            ' 解离常数转换为pKa
-            Return compound.DissociationConstants.Select(Function(k) -Math.Log10(k)).ToList()
-        End Function
-
-        ''' <summary>
-        ''' 获取化合物的简要描述
-        ''' </summary>
-        <System.Runtime.CompilerServices.Extension>
-        Public Function GetDescription(compound As Compound) As String
-            Dim sb As New StringBuilder()
-            sb.AppendLine($"化合物ID: {compound.Id}")
-            sb.AppendLine($"InChI Key: {compound.InChIKey}")
-            sb.AppendLine($"SMILES: {compound.Smiles}")
-            sb.AppendLine($"分子量: {compound.Mass:F3} g/mol")
-            sb.AppendLine($"分子式: {compound.GetMolecularFormula()}")
-            sb.AppendLine($"净电荷: {compound.GetNetCharge()}")
-
-            If compound.Microspecies IsNot Nothing AndAlso compound.Microspecies.Count > 0 Then
-                sb.AppendLine($"微物种数: {compound.Microspecies.Count}")
-            End If
-
-            If compound.GroupVector IsNot Nothing AndAlso compound.GroupVector.Length > 0 Then
-                sb.AppendLine($"基团向量维度: {compound.GroupVector.Length}")
-            End If
-
-            Return sb.ToString()
-        End Function
-
-    End Module
 
 End Namespace
